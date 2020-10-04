d1437fb48c4f55d4b26e7bd0dce3027302f07eaf
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-04 17:38:23 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,15 @@ fun main(){
     var name = "Przemysław"
     val surname = "Stokłosa"
 
-    var a = 10.0
-    a = 2.toDouble()
+    var a = 2.0
+    var b = 5.0
 
     println("Imię: "+name+" Nazwisko: "+surname)
     println("Imię: ${name} Nazwisko: ${surname}")
+
+    //2.0+5.0=7.0
+    val add = a+b
+
+    println(""+a+" + "+b+" = "+add)
+
 }
